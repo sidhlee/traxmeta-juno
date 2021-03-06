998d3055e50da522f9fe9c319cfb1e96ef78b329
Hayoun Lee <sidhlee@gmail.com>
2021-03-05 20:28:44 -0500

    feat: add markup & basic css

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,1 @@
 import './style.scss';
-import { Track } from './components/track';
-
-class Test {
-  static test() {
-    console.log('testing');
-  }
-}
-
-Test.test();
-Track.test();
-$('#app').text('hey');
-// console.log(uuid());
-
-console.log(process.env.API_LASTFM);
